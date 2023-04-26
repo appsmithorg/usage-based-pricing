104f8c52c38795d3c20235217e2495c1d2e8b874
vihar <vihar.kurama@gmail.com>
2023-04-26 20:54:08 +0530

    add next-seo

--- components/Layout.js
+++ components/Layout.js
@@ -1,37 +1,13 @@
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { NextSeo } from "next-seo";
 
 export default function Layout({ children }) {
   return (
     <>
-      <div className="">
-        {/* <Navbar
-          collapseOnSelect
-          expand="lg"
-          bg="light"
-          variant="dark"
-          className="border-bottom"
-        >
-          <Container>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mx-auto">
-                <Nav.Link className="text-dark" href="/">
-                  Home
-                </Nav.Link>
-                <Nav.Link
-                  className="text-dark"
-                  href="https://github.com/appsmithorg/usage-based-pricing-vendors"
-                  target="_blank"
-                >
-                  Contribute
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar> */}
-
-        <div className="">{children}</div>
-      </div>
+      <NextSeo
+        title="Usage Based Pricing Done Right"
+        description="Discover companies that put customers first with usage-based pricing"
+      />
+      <div className="">{children}</div>
     </>
   );
 }
